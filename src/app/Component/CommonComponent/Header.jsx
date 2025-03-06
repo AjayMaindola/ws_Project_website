39c5fb665b15061d8a1e858fe2940e79d7a061b6
AjayMaindola <ajay_maindola@yahoo.co.in>
2025-03-06 13:26:18 +0530

    glass effect

--- src/app/Component/CommonComponent/Header.jsx
+++ src/app/Component/CommonComponent/Header.jsx
@@ -32,9 +32,9 @@ export default function Header() {
    
   return (
     <>
-      <header className="bg-[url('/images/header/backgraound.jpg')] bg-no-repeat relative h-screen  ">
-        <div className="bg-black w-full z-[1] h-[90vh] opacity-50 absolute"></div>
-          <div className={`w-screen ${headerScrol ? "bg-teal-500 text-teal-100 md:text-black md:bg-white sticky top-0 z-[10] " : "relative z-[10] text-white " }`}>
+      <header className="bg-[url('/images/header/backgraound.jpg')] bg-cover  bg-no-repeat relative h-screen  ">
+        <div className="bg-black w-full z-[1] h-screen opacity-50 absolute"></div>
+          <div className={`w-screen ${headerScrol ? "bg-teal-500 text-teal-100 md:text-black md:bg-white/60 backdrop-blur-sm sticky top-0 z-[10] " : "relative z-[10] text-white " }`}>
               <div className=' max-w-[1340px] mx-auto pl-3 md:pl-[35px] lg:px-[10px] py-3 md:py-5 grid grid-cols-[60%_40%] md:gap-3 lg:gap-0 md:grid md:grid-cols-[15%_60%_20%] lg:grid lg:grid-cols-[15%_60%_auto] items-center'>
                 <div className=' text-2xl font-extrabold font-serif flex items-center md:justify-center gap-4 md:gap-4 '>
                   <Image src={logo} width={60} alt={logo} />
